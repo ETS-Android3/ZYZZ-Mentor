e6141f706022ae5eb00258ae4e3ba3e9c10a6a90
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-04-17 19:19:05 +0300

    finished home screen

---
+++ Android Studio/app/src/main/java/com/dhaini/zyzz/TrainerLogin.java
@@ -0,0 +1,14 @@
+package com.dhaini.zyzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrainerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer_login);
+    }
+}

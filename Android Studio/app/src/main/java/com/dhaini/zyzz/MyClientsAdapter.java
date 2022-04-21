cb4a1485bba597a383c13e02fdfcd578459ac935
mdhaini2 <mohamad.dhaini02@lau.edu>
2022-04-22 02:36:35 +0300

    Finished 70% of the work on recylerView

---
+++ Android Studio/app/src/main/java/com/dhaini/zyzz/MyClientsAdapter.java
@@ -0,0 +1,69 @@
+package com.dhaini.zyzz;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+import java.util.ArrayList;
+
+public class MyClientsAdapter extends RecyclerView.Adapter<MyClientsAdapter.myClientsViewHolder> {
+    private ArrayList<String> myClientsList;
+    private OnItemClickListener mListener;
+
+
+    public interface  OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+    }
+
+    public static class myClientsViewHolder extends RecyclerView.ViewHolder{
+        public TextView myClientTextView;
+        public myClientsViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            myClientTextView = itemView.findViewById(R.id.clientName);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(listener!=null){
+                        int position = getAdapterPosition();
+                        if(position!= RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+    public MyClientsAdapter(ArrayList<String> myClientsList){
+        this.myClientsList = myClientsList;
+    }
+    @NonNull
+    @Override
+    public myClientsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_client_item_recycler_view,parent,false);
+        myClientsViewHolder mCVH = new myClientsViewHolder(v,mListener);
+        return mCVH;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myClientsViewHolder holder, int position) {
+        String currentClient = myClientsList.get(position);
+        holder.myClientTextView.setText(currentClient);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return myClientsList.size();
+    }
+}
--- Android Studio/app/src/main/java/com/dhaini/zyzz/TrainerMyClients.java
+++ Android Studio/app/src/main/java/com/dhaini/zyzz/TrainerMyClients.java
@@ -1,6 +1,7 @@
 package com.dhaini.zyzz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -13,13 +14,17 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class TrainerMyClients extends AppCompatActivity {
     String trainerUsername;
-    private RecyclerView myClientListRecylerView;
-    
+    private RecyclerView myClientsListRecyclerView;
+    private MyClientsAdapter myClientsAdapter;
+    private RecyclerView.LayoutManager myClientsLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +32,28 @@ public class TrainerMyClients extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE); //hide the actionbar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+        setContentView(R.layout.activity_trainer_my_clients);
+
+        ArrayList<String> myClientsList = new ArrayList<>();
+
+        myClientsList.add("Mohamad Dhaini");
+        myClientsList.add("Ali Chehade");
+        myClientsList.add("Fadi Amad");
+        myClientsListRecyclerView = findViewById(R.id.myClientRecyclerView);
+
+
+        myClientsLayoutManager = new LinearLayoutManager(TrainerMyClients.this);
+        myClientsAdapter = new MyClientsAdapter(myClientsList);
+
+        myClientsListRecyclerView.setLayoutManager(myClientsLayoutManager);
+        myClientsListRecyclerView.setAdapter(myClientsAdapter);
+
 
 
         trainerUsername = getIntent().getStringExtra("trainerUsername");
 
         // Initializing Options Button
-        setContentView(R.layout.activity_trainer_my_clients);
+
         List<String> genders = Arrays.asList("","Add Client","Logout");
         Spinner optionsSpinner = findViewById(R.id.spinnerOption);
 
